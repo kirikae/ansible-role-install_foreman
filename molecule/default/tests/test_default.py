2a3ce00b3ac6df992b8b4d5efe1d875c308401c4
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-06-17 14:39:41 +1000

    Fix linting errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,10 +7,12 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_foreman_package(host):
     pkg = host.package('foreman-installer')
 
     assert pkg.is_installed
+
 
 def test_firewalld_service(host):
     srv = host.service('firewalld')
