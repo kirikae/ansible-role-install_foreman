f5480248123cb0b218232c2b58e5bbb978c159eb
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-06-17 13:34:25 +1000

    Update molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,23 +7,13 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-def test_hosts_file(host):
-    file = host.file('/etc/hosts')
-
-    assert file.exists
-    assert file.user == 'root'
-    assert file.group == 'root'
-
-
-def test_sudo_package(host):
-    pkg = host.package('sudo')
+def test_foreman_package(host):
+    pkg = host.package('foreman-installer')
 
     assert pkg.is_installed
-
 
 def test_firewalld_service(host):
     srv = host.service('firewalld')
 
     assert srv.is_running
-assert srv.is_enabled
+    assert srv.is_enabled
