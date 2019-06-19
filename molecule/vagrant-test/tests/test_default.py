ee73caea7c3ab21a39eca5178f561d1c727ac880
Kirikae <22813189+kirikae@users.noreply.github.com>
2019-06-20 09:17:22 +1000

    Update molecule configuration
    Remove Fedora from molecule.yml
    Rework some of the testinfra tests (more to be done..)

--- molecule/vagrant-test/tests/test_default.py
+++ molecule/vagrant-test/tests/test_default.py
@@ -8,14 +8,38 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_foreman_package(host):
-    pkg = host.package('foreman-installer')
+def test_packages(host):
+    pkg = host.package("foreman-installer")
 
     assert pkg.is_installed
+    # assert pkg.version.startswith(version)
 
 
-def test_firewalld_service(host):
-    srv = host.service('firewalld')
+# @pytest.mark.parametrize("port", "proto", [
+#     ("53", "tcp"),
+#     ("53", "udp"),
+#     ("67", "udp"),
+#     ("68", "udp"),
+#     ("69", "udp"),
+#     ("80", "tcp"),
+#     ("443", "tcp"),
+#     ("3000", "tcp"),
+#     ("3306", "tcp"),
+#     ("5432", "tcp"),
+#     ("8140", "tcp"),
+#     ("8443", "tcp"),
+# ])
+# def test_firewall(host, port, proto):
+#     svc = host.service('firewalld')
+#     socket = host.socket("{0}://{1}".format((proto, port)))
+#
+#     assert svc.is_running
+#     assert svc.is_enabled
+#     assert(socket.is_listening)
 
-    assert srv.is_running
-    assert srv.is_enabled
+
+# def test_foreman(host, File):
+#     f = host.File("/etc/ansible/facts.d/install_foreman.fact")
+#
+#     assert f.exists
+#     assert f.is_file
